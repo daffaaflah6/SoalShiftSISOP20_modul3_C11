a74b899abce84c7318a992d8fe1491503f7868fb
marshanabilah <marsha.nabilah@gmail.com>
2020-04-03 01:05:22 +0700

    revisi 4a

--- soal4/4a.c
+++ soal4/4a.c
@@ -6,11 +6,17 @@
 #include <sys/shm.h>
 #include <unistd.h>
 
+int *p;
 void multiplyMatrices(int matA[4][2], int matB[2][5], int multResult[4][5], int r1, int c1, int r2, int c2);
+
 
 int main() {
     int matA[4][2], matB[2][5], mult[4][5];
     int r1 = 4, c1 = 2, r2 = 2, c2 = 5;
+
+    key_t key = 1234;
+    int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+    p = shmat(shmid, NULL, 0);
 
     matA[0][0]=1;
     matA[0][1]=2;
@@ -34,25 +40,16 @@ int main() {
 
     multiplyMatrices(matA, matB, mult, r1, c1, r2, c2);
 
+    shmdt(p);
+    shmctl(shmid, IPC_RMID, NULL);
+    
     return 0;
 }
 
 
 void multiplyMatrices(int matA[4][2], int matB[2][5], int mult[4][5], int r1, int c1, int r2, int c2) {
-
-    int *p;
-    key_t key = 1234;
-    int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-    p = shmat(shmid, NULL, 0);
-    int hasil;
+    int hasil = 0;
     
-    for (int i = 0; i < r1; ++i) {
-        for (int j = 0; j < c2; ++j) {
-            mult[i][j] = 0;
-        }
-    }
-
-    // Multiplying matA and matB matrices and storing in mult.
     for (int i = 0; i < r1; ++i) {
         for (int j = 0; j < c2; ++j) {
             for (int k = 0; k < c1; ++k) {
