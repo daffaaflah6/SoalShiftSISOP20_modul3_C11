a9801f2e3c3515ec11f663fb918c905d5d48b5af
Muhammmad Daffa' Aflah Syarif <52326074+daffaaflah6@users.noreply.github.com>
2020-04-03 14:30:12 +0700

    no 2 belum selesai

--- soal2/soal2-server.c
+++ soal2/soal2-server.c
@@ -21,7 +21,7 @@ int HP1 = 100, HP2 = 100;
 
 void sc2Handler(int new_socket);
 void* sc1Handler (void* args);
-void playHandler()
+void playHandler();
 
 int main(int argc, char const *argv[]){
     int server_fd, new_socket, val;
@@ -70,66 +70,65 @@ int main(int argc, char const *argv[]){
 }
 
 void sc2Handler(int new_socket){
-  printf("new game--------------------\n");
-  HP1 = 100;
-  HP2 = 100;
+    printf("new game--------------------\n");
+    HP1 = 100;
+    HP2 = 100;
 
-  int val;
-  char findOrLogout[100] = "";
-  val = read( new_socket , findOrLogout, 1024);
-  if(strcmp(findOrLogout, "find") == 0){
-    koneksi_pemain++;
-    while(koneksi_pemain < 2){
-      //wait
-      char mess[100] = "Waiting for player ...";
-      send(new_socket , mess , strlen(mess) , 0 );
-      usleep(100000);
-    }
-    char mess[100] = "mulai";
-    send(new_socket , mess , strlen(mess) , 0 );
+    int val;
+    char findOrLogout[100] = "";
+    val = read( new_socket , findOrLogout, 1024);
+    if(strcmp(findOrLogout, "find") == 0){
+        koneksi_pemain++;
+        while(koneksi_pemain < 2){
+            //wait
+            char mess[100] = "Waiting for player ...";
+            send(new_socket , mess , strlen(mess) , 0 );
+            usleep(100000);
+        }
+        char mess[100] = "mulai";
+        send(new_socket , mess , strlen(mess) , 0 );
 
-    char menang[100] = "End Game, You WIN";
-    while(true){
-      char hit[100], hitSend[100] = "hit";
-      val = read( new_socket , hit, 1024);
-      if(HP2 <= 0 || HP1 <= 0)
-        break;
-      if(new_socket == sock_id[0]){
-        HP2 = HP - 10;
-        if(HP2 <= 0){
-          strcpy(hitSend, "End Game, You WIN");
-          send(sock_id[1] , hitSend , strlen(hitSend) , 0 );
-          send(sock_id[0] , menang , strlen(menang) , 0 );
-          break;
+        char menang[100] = "End Game, You WIN";
+        for(;;){
+            char hit[100], hitSend[100] = "hit";
+            val = read( new_socket , hit, 1024);
+            if(HP2 <= 0 || HP1 <= 0) break;
+            if(new_socket == sock_id[0]){
+                HP2 = HP - 10;
+                if(HP2 <= 0){
+                strcpy(hitSend, "End Game, You WIN");
+                send(sock_id[1] , hitSend , strlen(hitSend) , 0 );
+                send(sock_id[0] , menang , strlen(menang) , 0 );
+                break;
+                }
+                //send to 1
+                printf("hit\n");
+                send(sock_id[1] , hitSend , strlen(hitSend) , 0 );
+            }
+            else if (new_socket == sock_id[1]){
+                HP1 = HP - 10;
+                if(HP1 <= 0){
+                    strcpy(hitSend, "End Game, You LOSE");
+                    send(sock_id[0] , hitSend , strlen(hitSend) , 0 );
+                    send(sock_id[1] , menang , strlen(menang) , 0 );
+                    break;
+                }
+                //send to 0
+                printf("hit\n");
+                send(sock_id[0] , hitSend , strlen(hitSend) , 0 );
+            }
         }
-        //send to 1
-        printf("hit\n");
-        send(sock_id[1] , hitSend , strlen(hitSend) , 0 );
-      }
-      else if (new_socket == sock_id[1]){
-        HP1 = HP - 10;
-        if(HP1 <= 0){
-          strcpy(hitSend, "End Game, You LOSE");
-          send(sock_id[0] , hitSend , strlen(hitSend) , 0 );
-          send(sock_id[1] , menang , strlen(menang) , 0 );
-          break;
-        }
-        //send to 0
-        printf("hit\n");
-        send(sock_id[0] , hitSend , strlen(hitSend) , 0 );
-      }
     }
-  }
-  else if(strcmp(findOrLogout, "logout") == 0){
-    sc1Handler(&new_socket);
-  }
-  else{
+    else if(strcmp(findOrLogout, "logout") == 0){
+        sc1Handler(&new_socket);
+    }
+    else{
+        sc2Handler(new_socket);
+        return;
+    }
+    sleep(1);
+    koneksi_pemain = 0;
     sc2Handler(new_socket);
-    return;
-  }
-  sleep(1);
-  koneksi_pemain = 0;
-  sc2Handler(new_socket);
 }
 
 void* sc1Handler (void* args){
@@ -149,10 +148,9 @@ void* sc1Handler (void* args){
     printf("%s\n", id.user);
     printf("%s\n", id.passw);
 
-    FILE *f;
-    f = fopen("akun.txt", "a+");
-    if(strcmp(loginRegister, "login") == 0)
-    {
+    FILE *f1;
+    f1 = fopen("akun.txt", "a+");
+    if(strcmp(loginRegister, "login") == 0){
         struct ident cek_ID;
         bool loginSuccess = false;
         char loginCheck[100] = "";
